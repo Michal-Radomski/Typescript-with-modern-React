082aeb2eb6dd1ab6a3c1ebdbbc5535754b52f776
Michal Radomski <michalra@op.pl>
2021-09-29 21:17:00 +0200

    App 1 - ToDo List done

--- App1-ToDo_List/src/App.tsx
+++ App1-ToDo_List/src/App.tsx
@@ -1,16 +1,65 @@
 import React from "react";
-import TestComponent from "./TestComponent";
+
+type FormElem = React.FormEvent<HTMLFormElement>;
+interface ToDo {
+  text: string;
+  complete: boolean;
+}
 
 function App() {
-  function sum(a: number, b: number): number {
-    return a + b;
-  }
+  const [value, setValue] = React.useState<string>("");
+  const [todos, setTodos] = React.useState<ToDo[]>([]);
+
+  const handleSubmit = (e: FormElem): void => {
+    e.preventDefault();
+    // console.log(value);
+    addToDo(value);
+    setValue("");
+  };
+
+  const addToDo = (text: string): void => {
+    const newTodos: ToDo[] = [...todos, {text, complete: false}];
+    setTodos(newTodos);
+  };
+
+  const completeToDo = (index: number): void => {
+    const newTodos: ToDo[] = [...todos];
+    newTodos[index].complete = !newTodos[index].complete;
+    setTodos(newTodos);
+  };
+
+  const removeTodo = (index: number): void => {
+    const newTodos: ToDo[] = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  console.log(todos);
+  // debugger;
+
   return (
     <React.Fragment>
-      <p>Hello TypeScript!</p>
-      <p>TSX Component</p>
-      <p>Sum function: sum 2+15: {sum(2, 15)}</p>
-      <TestComponent />
+      <h1>ToDo List</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" required placeholder="Enter here..." value={value} onChange={(e) => setValue(e.target.value)} />
+        <button type="submit">Add ToDo</button>
+      </form>
+      <section>
+        {todos.map((todo: ToDo, index: number) => {
+          return (
+            <div style={{backgroundColor: "whiteSmoke"}} key={index}>
+              <div style={{textDecoration: todo.complete ? "line-through" : "underLine"}}>{todo.text}</div>
+              <button type="button" onClick={(): void => completeToDo(index)}>
+                {todo.complete ? "Incomplete" : "Complete"}
+              </button>
+              <button type="button" onClick={(): void => removeTodo(index)}>
+                Remove
+              </button>
+              <hr />
+            </div>
+          );
+        })}
+      </section>
     </React.Fragment>
   );
 }
--- App1-ToDo_List/src/TestComponent.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const TestComponent = () => {
-  return <div>JSX Component</div>;
-};
-
-export default TestComponent;
